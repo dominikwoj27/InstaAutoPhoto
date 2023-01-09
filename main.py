219a749a7444168b322fc176c2b2d47c4abcc195
dominikwoj27 <dominik.wojciechowski@fortech21.pl>
2023-01-09 09:26:29 +0100

    instagrApi added

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+import time
 import getLines
-    
+from instagrapi import Client
+
+client = Client()
+client.login('kkatskii', 'Mietek97')
+
 getLinesFromFile = getLines.GetLinesFromFile()
 listOfAccounts = getLinesFromFile.GetLinesToList()
 
-for i in range(len(listOfAccounts)):
-    print (listOfAccounts[i])
+for account in range(len(listOfAccounts)):
+    print('starting liking pics of account: ', listOfAccounts[account])
+    userId = client.user_id_from_username(listOfAccounts[account])
+    userMediasList = client.user_medias(userId, 10, 1)
+    
+    for item in range(len(userMediasList)):
+        client.media_like(userMediasList[item].id)
+        print('liked image with caption:', userMediasList[item].caption_text)
+        time.sleep(1)
     
